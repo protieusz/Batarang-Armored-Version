068e0f790b49e953313495757601e5403afde358
Protieus <118025702+protieusz@users.noreply.github.com>
2023-08-05 13:49:06 -0700

    QMK Source code

---
+++ batarangarmored/batarangarmored.c
@@ -0,0 +1,1 @@
+#include "batarangarmored.h"
---
+++ batarangarmored/rules.mk
@@ -0,0 +1,6 @@
+WS2812_DRIVER = vendor
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_spi
+# RP2040-specific options
+PICO_INTRINSICS_ENABLED = no # ATM Unsupported by ChibiOS.
+ENCODER_MAP_ENABLE = yes
